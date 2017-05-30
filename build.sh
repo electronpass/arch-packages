cd89667ba416db3aa19aafa946131a7c30bd370f
Ziga Patacko Koderman <ziga.patacko@protonmail.com>
2017-05-30 14:49:31 +0200

    fix faulty path in build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 # build and upload libelectronpass package
 cd libelectronpass-pkgbuild
 makepkg
-curl --upload-file ./libelectronpass*.pkg.tar https://transfer.sh/
+curl --upload-file ./libelectronpass-pkgbuild/*.pkg.tar https://transfer.sh/
