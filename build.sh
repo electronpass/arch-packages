54533df170f389599538abfb7fb8b4f600be39a5
Ziga Patacko Koderman <ziga.patacko@protonmail.com>
2017-05-30 15:08:20 +0200

    update build system

--- build.sh
+++ build.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
 
-# build and upload libelectronpass package
+# build libelectronpass package
 cd libelectronpass-pkgbuild
 makepkg
-curl --upload-file ./libelectronpass-pkgbuild/*.pkg.tar https://transfer.sh/
+cd ..
+
+# install libelectronpass
+sudo pacman -U --noconfirm libelectronpass-pkgbuild/*.pkg.tar
+
+# build electronpass-desktop package
+cd electronpass-desktop-pkgbuild
+makepkg
+cd ..
+
+# create arch database files
+repo-add electronpass-current.db.tar.gz libelectronpass-pkgbuild/*.pkg.tar electronpass-desktop-pkgbuild/*.pkg.tar
+
+# create release dir, copy all files into it, compress it and upload to transfer.sh
+mkdir release
+cp electronpass-current* release/
+cp libelectronpass-pkgbuild/*pkg.tar release/
+cp electronpass-desktop-pkgbuild/*.pkg.tar release/
+tar czf electronpass-current-repo.tar.gz release/
+curl --upload-file ./electronpass-current-repo.tar.gz https://transfer.sh/electronpass-current-repo.$(git rev-parse --short HEAD)-x86_64.tar.gz
